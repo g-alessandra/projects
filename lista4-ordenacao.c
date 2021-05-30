aa59075e9e1b358838ec3d04fcf6949976090808
g-alessandra <80726328+g-alessandra@users.noreply.github.com>
2021-05-30 01:10:15 -0300

    Update lista4-ordenacao.c

--- lista4-ordenacao.c
+++ lista4-ordenacao.c
@@ -1,13 +1,3 @@
-/*
-1- Implementar os algoritmos de ordenação Insertion Sort, Selection Sort, Quick Sort e Merge Sort.
-2- O programa deve solicitar o tamanho do vetor de números (n) que será ordenado
-3- O programa deve preencher um vetor de tamanho n com números aleatórios. Usar srand(time(NULL)); no inicio do programa.
-4- Faça um código que verifica se o resultado dado por cada algoritmo de ordenação está correto.
-5- Use a melhor lib para calcular quanto tempo cada algoritmo de ordenação levou para terminar:
-https://levelup.gitconnected.com/8-ways-to-measure-execution-time-in-c-c-48634458d0f9
-6- No 1:1 vou perguntar sobre questões de desempenho relacionadas a esse exercício, então pense sobre desempenho, otimizações, avaliação de tempo, complexidade, hardware, etc
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -40,10 +30,10 @@ void Verifica(int *vetor, int tam) {
         }
     }
     if(erro == 0){
-        printf("O resultado está correto!\n");
+        printf("\nO resultado está correto!\n");
     }
     else{ 
-        printf("O resultado está incorreto!\n");
+        printf("\nO resultado está incorreto!\n");
     }
 }
 
